f65c64787dcba332d2874ebf0c4eecc2c5a2ff6f
Gabe DuBose <gabe@Gabes-MacBook-Air.local>
2024-08-26 13:21:25 -0400

    IRe-did website

--- style.js
+++
@@ -1,9 +0,0 @@
-
-function changeColor() {
-    var headText = document.getElementById("asciiHeader");
-    var mainColor = "rgb(250, 203, 106)";
-    var secondColor = "rgb(234,250,250)"
-    headText.style.color = headText.style.color == mainColor?secondColor:mainColor;
-}
-
-window.setInterval(changeColor,800);
